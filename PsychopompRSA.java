e127060a4e6b4a4db4e181c0ded139d2c1a6561b
definitelyafellowhuman <n451.1356@gmail.com>
2026-09-17 19:23:43 +0200

    Refactor PsychopompRSA by removing dead code
    Removed unused methods and commented-out code to clean up PsychopompRSA.java.

--- PsychopompRSA.java
+++ PsychopompRSA.java
@@ -18,10 +18,6 @@ public class PsychopompRSA {
         System.out.println(returnVal);
     }
 
-    public void autoTestRSA (BigInteger bigInt) {
-
-    }
-
     public void encryptFile (File f) throws IOException {
         encryptFile(f, f);
     }
@@ -73,7 +69,6 @@ public class PsychopompRSA {
     // core
     public void decryptFile (File srcFile, File decFile) throws IOException {
         BigInteger fileData = readTxtAsBigInt( srcFile );
-        // message = fileData.modPow( this.decryptionKey, this.modulusN );
         BigInteger message = fileData.modPow(decryptionKey, modulusN);
         writeBigIntegerFile( decFile, message );
 
@@ -107,53 +102,12 @@ public class PsychopompRSA {
             bigInteger = bigInteger.subtract( BigInteger.valueOf(nextInt).shiftLeft( shift ) );
         }
 
-        // returnValue.append((char) bigInteger.intValueExact());
-
         return returnValue.toString();
     }
-
-    /*
-    public static String getFileTxt(String fileURL) throws IOException {
-        return getFileTxt(new File(fileURL));
-    }
-
-    public static String getFileTxt(File file) throws IOException {
-        FileReader fileReader = new FileReader(file);
-        StringBuilder stringBuilder = new StringBuilder();
-        int c = fileReader.read();
-
-        while (c > -1 && c != 65535) {
-            stringBuilder.append((char) c);
-            c = (char) fileReader.read();
-        }
-
-        fileReader.close();
-        return stringBuilder.toString();
-    }
-
-    public static byte[] getFileBytes(File file) throws IOException {
-        return Files.readAllBytes(file.toPath()); // TODO i should probably implement this instead of getFileTxt
-    }
-
-    public static void setFileTxt(File file, char[] chars) throws IOException {
-        FileWriter fileWriter = new FileWriter(file);
-        fileWriter.write(chars);
-        fileWriter.flush();
-        fileWriter.close();
-    }
-
-    public static void setFileTxt(String fileURL, char[] chars) throws IOException {
-        FileWriter fileWriter = new FileWriter(fileURL);
-        fileWriter.write(chars);
-        fileWriter.flush();
-        fileWriter.close();
-    }
-    */
 
     public static BigInteger readBigIntegerFile(File file) throws FileNotFoundException {
 
         BigInteger bigInteger = BigInteger.ZERO;
-
 
         try (FileReader fileReader = new FileReader( file )) {
 
@@ -181,12 +135,9 @@ public class PsychopompRSA {
             while (bigInteger.bitLength() > 0) {
 
                 shift = bigInteger.bitLength() - (bigInteger.bitLength() - 1) % 16 - 1;
-                // System.out.println(bigInteger.shiftRight( shift ));
                 next2bytes = bigInteger.shiftRight( shift ).intValueExact();
                 bigInteger = bigInteger.subtract( BigInteger.valueOf( next2bytes ).shiftLeft( shift ) );
                 fileWriter.append( (char) next2bytes );
-
-                // System.out.println( "debug " + (char) next2bytes );
 
             }
         }
@@ -284,7 +235,6 @@ public class PsychopompRSA {
     }
 
     public void generateKeys (BigInteger p, BigInteger q, boolean ctrl) {
-        // 1000000000000066600000000000001
 
         BigInteger e, d, n, lambdaN;
 
@@ -294,7 +244,7 @@ public class PsychopompRSA {
         // lambda(n) = lambda(pq) = lcm(lambda(p),lambda(q)) = lcm((p-1),(q-1)); lambda is the Carmichael function;
         // lambdaN = ( p.subtract(BigInteger.ONE) ).gcd( q.subtract(BigInteger.ONE) ); // idk why i need this
 
-        // it just is
+        // it just is.
         e = BigInteger.valueOf(65537);
 
         // TODO: replace legacy code because the function literally checks every number until it finds a suitable one
@@ -331,11 +281,6 @@ public class PsychopompRSA {
                 if (mod.compareTo(BigInteger.ONE) == 0) {       // probably redundant if-statement
                     possibleKeys.add(d);
 
-                    /* debug    System.out.println(i + " - " + possibleKeys.size() + " - " + d);
-                    // System.out.println("-------" + d + "-------");
-                    // System.out.println(d + " = " + i + " * " + e + "    " + possibleKeys);
-                    // System.out.println(mod);
-                    // */
                 }
             }
 
@@ -359,15 +304,10 @@ public class PsychopompRSA {
         BigInteger e;  // encryption key
         BigInteger d;  // decryption key
         BigInteger n;
-        // unnecessary  BigInteger phiN;
-        // BigInteger upperBound = BigInteger.valueOf(100_000);    // can be adjusted
-        // BigInteger lowerBound = BigInteger.valueOf(10_000);
         BigInteger range = upperBound.subtract(lowerBound);
         BigInteger[] returnMe = new BigInteger[3];
         final BigInteger[] primes = new PsychopompPrimes().makeshiftBigSieveOfEratosthenes(0x7FFF_FFFF);
         int lowestOrigin = primes.length;   // for use in 'rng.nextInt(lowestOrigin, primes.length - 1)'
-
-        // debug    System.out.println(Arrays.toString(primes));
 
         while (primes[lowestOrigin - 1].compareTo(lowerBound) > 0) {
             lowestOrigin--; // the lowest selectable number's index to be used in 'rng.nextInt(lowestOrigin, primes.length - 1)'
@@ -381,31 +321,12 @@ public class PsychopompRSA {
         // note:    while (primeQ > (primeP - (range / 4) ) && primeQ < (primeP + (range / 4) ))    ensures that the chosen primes aren't too similar
 
         n = primeP.multiply(primeQ);
-        // unnecessary  phiN = (primeP.subtract(BigInteger.ONE)).multiply(primeQ.subtract(BigInteger.ONE));
 
         e = BigInteger.valueOf(65537);
         d = legacy_generateDecryptionKeys(primeP, primeQ, e, 1)[0];
 
-        // debug    System.out.println(Arrays.toString(generateDecryptionKeys(primeP, primeQ, e, 15)));
-
-        /* debug
-        System.out.println("\ne " + e);
-        System.out.println("d " + d);
-
-        BigInteger x = BigInteger.valueOf(123574567);
-        BigInteger y = x.modPow(e, n);
-        BigInteger z = y.modPow(d, n);
-        System.out.println("x " + x);
-        System.out.println("y " + y);
-        System.out.println("z " + z);
-
-        System.out.println("\n" + e.multiply(d));
-        System.out.println(phiN);
-        System.out.println("" + (e.multiply(d)).mod(phiN) + '\n');
-        // */
-
-        System.out.println("encryption(" + e + ", " + n + ")");
-        System.out.println("decryption(" + d + ", " + n + ")");
+        // System.out.println("encryption(" + e + ", " + n + ")");
+        // System.out.println("decryption(" + d + ", " + n + ")");
 
         returnMe[0] = e;
         returnMe[1] = d;
